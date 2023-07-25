b99d8133534a7ad78d386aea009f15483b1bb0d2
Teye Odonkor <odonkorteye@yahoo.com>
2023-07-25 04:10:47 -0700

    files updated

--- main.h
+++ main.h
@@ -1,8 +1,8 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdarg.h>
-#define MAIN_H
 #ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
